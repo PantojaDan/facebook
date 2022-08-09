d7e7b66dd0a182b59e3fb19550443f686bdbea9d
Daniel <biyuyisdan@gmail.com>
2022-08-08 22:59:02 -0500

    Se ha agregado la conexion a database remota

--- connect.php
+++ connect.php
@@ -2,11 +2,14 @@
 
 try{    
     $conn = mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'credentials'
+        'us-cdbr-east-06.cleardb.net',
+        'b04c7a660a6f0e',
+        'ea9b1ec1',
+        'heroku_43221ca6dc1deae'
     );
+    if($conn){
+        echo "connected";
+    }
 }catch(Exception $e){
     echo "Error ".$e->getMessage();
 }
